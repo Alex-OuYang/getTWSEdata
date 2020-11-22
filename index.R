118d720fd683806ca0685732e26caf7ed0e156ce
OU-YANG-JI-HAI <alex1101137204@gmail.com>
2020-11-22 20:44:11 +0800

    source path change

--- index.R
+++ index.R
@@ -5,9 +5,13 @@ nowDate=paste0(nowDateSplit[[1]][1],nowDateSplit[[1]][2],nowDateSplit[[1]][3])
 startDate="20200101"
 endDate=nowDate
 
+encodingStr="utf-8"
+
+baseDataPathStr=paste0(projectPathStr,"BasicData/ShowBasicData.R")
+getCompanyPathStr=paste0(projectPathStr,"BasicData/GetCompanyList.R")
 
 # Download Basic Data
-# source("GetCompanyList.R")
+source(getCompanyPathStr,encoding = encodingStr)
 # get all company list
 # stocknolist=c(companylist)
 # stocknolist=c(companylist[1:length(companylist)])
@@ -20,9 +24,8 @@ endDate=nowDate
 # downloadData(stocknolist)
 
 # Show Basic Data
-stockno="2882"
-pathStr=paste0(projectPathStr,"BasicData/ShowBasicData.R")
-source(pathStr,encoding="utf-8")
+stockno="2330"
+source(baseDataPathStr,encodingStr)
 # addBBands(n=20,sd=2,maType = "EMA")
 # addEMA(n=20,col = "blue")
 # addEMA(n=60,col = "purple")
